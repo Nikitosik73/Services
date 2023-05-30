b9caf8f4fa868bd407db337462fd2aeb3c4f1644
nikita <paramonov20@bk.ru>
2023-05-30 23:44:52 +0300

    Изучил foregroundService и создал свой первый foregroundService

--- app/src/main/java/com/example/services/MainActivity.kt
+++ app/src/main/java/com/example/services/MainActivity.kt
@@ -8,11 +8,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
 import com.example.services.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    private var id = 0
 
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
@@ -27,42 +26,10 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.foregroundService.setOnClickListener {
-            showNotification()
-        }
-    }
-
-
-    private fun showNotification() {
-
-        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val notificationChannel = NotificationChannel(
-                CHANNEL_ID,
-                CHANNEL_NAME,
-                NotificationManager.IMPORTANCE_DEFAULT
+            ContextCompat.startForegroundService(
+                this,
+                MyForegroundService.newIntent(this)
             )
-            notificationManager.createNotificationChannel(notificationChannel)
-        }
-        /*
-            Два варианта создания уведомлания, либо через проверку на sdk
-            либо использовать NotificationCompat, где проверка на sdk
-            выполняется под капотом
-         */
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val notification = Notification.Builder(this, CHANNEL_ID)
-                .setContentTitle("title")
-                .setContentText("text")
-                .setSmallIcon(R.drawable.ic_launcher_background)
-                .build()
-
-            notificationManager.notify(id++, notification)
         }
     }
-
-    companion object {
-        private const val CHANNEL_ID = "channel_id"
-        private const val CHANNEL_NAME = "channel_name"
-    }
-
 }
