700176c3d6b1cb48badc08c4719b946263a2cb6a
nikita <paramonov20@bk.ru>
2023-05-31 00:22:06 +0300

    Изучил два способа остановки сервисов

--- app/src/main/java/com/example/services/MyForegroundService.kt
+++ app/src/main/java/com/example/services/MyForegroundService.kt
@@ -26,10 +26,11 @@ class MyForegroundService : Service() {
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         log("onStartCommand")
         coroutineScope.launch {
-            for (i in 0 until 100) {
+            for (i in 0 until 10) {
                 delay(1000)
                 log("Timer $i")
             }
+            stopSelf() // остановка внутри сервиса
         }
         return START_STICKY
     }
